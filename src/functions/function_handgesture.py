dc94ab958f94f7f942b11c744c1b7230575b16ae
otter <1369460359@qq.com>
2023-04-11 20:11:59 +0800

    update handgesture tts

--- src/functions/function_handgesture.py
+++ src/functions/function_handgesture.py
@@ -1,5 +1,6 @@
 import typing
 
+from external.tts import TTS
 from functions.function_base import FunctionBase
 from utils.feature import FeatureDict
 
@@ -10,6 +11,7 @@ class FunctionHandGesture(FunctionBase):
         self.handgesture_response = {"good": "好", "bad": "坏", "5": "挥手", "2": "耶"}
         self.last_call_time = 0
         self.interval_time = 20
+        self.tts = TTS()
 
     def check(self, features: typing.List[FeatureDict], current_time: int) -> bool:
         if current_time - self.last_call_time < self.interval_time:
@@ -21,11 +23,14 @@ class FunctionHandGesture(FunctionBase):
         if len(my_features) == 1:
             """如果是刚得到的，就接受，否则拒绝"""
             if current_time <= my_features[0]["timestamp"]:
-                label = my_features[0]["data"]["gesture"]
+                label = my_features[0]["data"]["gesture"].lower()
                 return label in self.handgesture_response.keys()
             else:
                 return False
         else:
+            return False
+        tts_text = self.handgesture_response[my_features[0]["data"]["gesture"]]
+        if self.tts.run(tts_text) != 0:
             return False
 
     def call(self, features: typing.List[FeatureDict], current_time: int):
@@ -37,4 +42,5 @@ class FunctionHandGesture(FunctionBase):
             my_feature["data"]["gesture"]
         ]  # 使用label作为key
         self.last_call_time = my_feature["timestamp"]
-        return response
+        res = self.tts.run(response)
+        return res
